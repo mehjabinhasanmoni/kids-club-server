efb8a21425849b3ec16346cfd9a51282603e420e
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-06-11 08:34:05 +0600

    make admin API created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -33,11 +33,39 @@ async function run() {
 
     // Users related APIS
 
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
     app.post('/users', async (req, res) => {
       const user = req.body;
+      const query = {email : user.email}
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: "User already exists"})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+
 
 
     // Class related APIS
