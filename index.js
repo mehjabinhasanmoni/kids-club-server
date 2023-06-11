97a507cc971994e62a722efe56d602f571e5f737
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-06-11 19:17:57 +0600

    JWT middleware setup

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 
 
@@ -32,7 +49,7 @@ async function run() {
     const usersCollection = client.db("kidsClubDB").collection("users");
 
     // JWT 
-    
+
     app.post('/jwt', (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
